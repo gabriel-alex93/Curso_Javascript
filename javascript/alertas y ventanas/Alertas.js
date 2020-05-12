2ce44bb22ee5991cc1f9d4d2ec7be34f55d64d01
gabriel-alex93 <gabo9302@gmail.com>
2020-05-12 14:31:50 -0400

    agrega clases 29 30 31

---
+++ javascript/alertas y ventanas/Alertas.js
@@ -0,0 +1,14 @@
+'use strict'
+ 
+//Alertas y ventanas
+//Alerta
+alert("Esta es mi alerta");
+alert("este es mi texto");
+
+// alertas de confirmacion
+var resultado = confirm("estas seguro de querer continuar");
+console.log(resultado);
+
+//ingreso de datos
+var mi_resultado = prompt("¿que edad tienes?",18);//el segundo parametro indica valor por defecto
+console.log(mi_resultado);//(sera un string se debe convertir en caso de)
---
+++ javascript/do while/doWhile.js
@@ -0,0 +1,10 @@
+'use strict'
+ 
+//while
+var years = 21;
+do{	
+	alert("Solo cuando sea diferente a 20");
+	years++;
+
+}while(years > 20)
+
